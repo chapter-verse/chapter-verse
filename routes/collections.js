586dfdc435d1b9a68704797bb04aad56695257fd
valyy151 <marin.valenta1@gmail.com>
2023-03-29 12:20:44 +0200

    Collections now render each book

--- routes/collections.js
+++ routes/collections.js
@@ -30,30 +30,41 @@ const fileUploader = require('../config/cloudinary.config');
 // 	});
 
 router.post('/create', fileUploader.single('cover'), (req, res, next) => {
-    console.log(req)
-    const {username} = req.session.currentUser;
+	console.log(req);
+	const { username } = req.session.currentUser;
 	const { name, description } = req.body;
-    Collection.create({name, description, user: req.session.currentUser, imageUrl: req.file.path}, { new: true })
-        .then(() => {
-            // User.findOne(username)
-            // .then(() => {
-            //     user.collections.push(collection);
-                res.redirect(`/profile/${currentUser}`);
-            // });
-        })
-        .catch((err) => {
-            console.log(err);
-            next(err);
-        });
+	Collection.create({ name, description, user: req.session.currentUser, imageUrl: req.file.path }, { new: true })
+		.then(() => {
+			// User.findOne(username)
+			// .then(() => {
+			//     user.collections.push(collection);
+			res.redirect(`/profile/${currentUser}`);
+			// });
+		})
+		.catch((err) => {
+			console.log(err);
+			next(err);
+		});
 });
 
-
-router.get('/:collectionsId', (req, res) => {
-	const { collectionsId } = req.params;
-	Collection.findById(collectionsId)
-		.populate('books')
-		.then((data) => res.render('collection', data))
-		.catch((err) => console.log(err));
+router.get('/:collectionsId', async (req, res) => {
+	try {
+		const { collectionsId } = req.params;
+		const collection = await Collection.findById(collectionsId).populate('books');
+		const bookData = await Promise.all(
+			collection.books.map(async (book) => {
+				const response = await axios.get(`https://www.googleapis.com/books/v1/volumes/${book}`, {
+					headers: {
+						'Referrer-Policy': 'no-referrer-when-downgrade',
+					},
+				});
+				return response.data;
+			}),
+		);
+		res.render('collection', { collection, books: bookData });
+	} catch (error) {
+		console.error(error);
+	}
 });
 
 router.post('/:collectionsId/edit', (req, res, next) => {
@@ -70,9 +81,9 @@ router.post('/:collectionsId/edit', (req, res, next) => {
 		});
 });
 
-router.post('/:collectionsId/edit-cover',fileUploader.single('cover'), (req, res, next) => {
+router.post('/:collectionsId/edit-cover', fileUploader.single('cover'), (req, res, next) => {
 	const { collectionsId } = req.params;
-	Collection.findByIdAndUpdate(collectionsId, {imageUrl: req.file.path}, { new: true })
+	Collection.findByIdAndUpdate(collectionsId, { imageUrl: req.file.path }, { new: true })
 		.then(() => {
 			res.redirect(`/collections/${collectionsId}`);
 		})

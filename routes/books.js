c0f4ca641b125390bf0db2dae852b6c0d23a8953
Canacepa <cassandre.lavallard@gmail.com>
2023-03-29 18:47:08 +0200

    collection button if not user

--- routes/books.js
+++ routes/books.js
@@ -94,4 +94,29 @@ router
 		res.redirect(`/books/${bookId}`);
 	});
 
+router.post('/:bookId/delete', (req, res, next) => {
+	const { bookId } = req.params;
+	console.log(req)
+	const collectionId =
+	Collection.findById(collectionsId)
+		.then(deletedCollection => {
+			return User.findOne({ username: currentUser })
+				.populate('collections')
+				.then(user => {
+					const collectionToDeleteIndex = user.collections.findIndex(collection => collection._id.equals(deletedCollection._id));
+					if (collectionToDeleteIndex !== -1) {
+						user.collections.splice(collectionToDeleteIndex, 1);
+						return user.save();
+					}
+				})
+				.then(() => {
+					res.redirect('/user/profile');
+				});
+		})
+		.catch(error => {
+			console.log(error);
+			next(error);
+		});
+});
+
 module.exports = router;
--- routes/collections.js
+++ routes/collections.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const bcrypt = require('bcrypt');
 const axios = require('axios');
 const fileUploader = require('../config/cloudinary.config');
+const { collection } = require('../models/User.model');
 
 router
 	.route('/create')
@@ -29,42 +30,36 @@ router
 		}
 	});
 
-// router.post('/create', fileUploader.single('cover'), (req, res, next) => {
-// 	console.log(req);
-// 	const { username } = req.session.currentUser;
-// 	const { name, description } = req.body;
-// 	Collection.create({ name, description, user: req.session.currentUser, imageUrl: req.file.path }, { new: true })
-// 		.then(() => {
-// 			// User.findOne(username)
-// 			// .then(() => {
-// 			//     user.collections.push(collection);
-// 			res.redirect(`/profile/${currentUser}`);
-// 			// });
-// 		})
-// 		.catch((err) => {
-// 			console.log(err);
-// 			next(err);
-// 		});
-// });
-
-router.get('/:collectionsId', async (req, res) => {
-	try {
-		const { collectionsId } = req.params;
-		const collection = await Collection.findById(collectionsId).populate('books');
-		const bookData = await Promise.all(
-			collection.books.map(async (book) => {
-				const response = await axios.get(`https://www.googleapis.com/books/v1/volumes/${book}`, {
-					headers: {
-						'Referrer-Policy': 'no-referrer-when-downgrade',
-					},
-				});
-				return response.data;
-			}),
-		);
-		res.render('collection', { collection, books: bookData });
-	} catch (error) {
-		console.error(error);
-	}
+router.get('/:collectionsId', (req, res) => {
+    let verificationObject = {};
+    const { currentUser } = req.session;
+    const { collectionsId } = req.params;
+    
+    Collection.findById(collectionsId)
+        .populate('books')
+        .then(collection => {
+            const user = collection.user;
+            return User.findById(user).then(person => {
+                if (person.username === currentUser.username) {
+                    verificationObject.property = 'Verified!';
+                }
+                
+                return Promise.all(collection.books.map(book => {
+                    return axios.get(`https://www.googleapis.com/books/v1/volumes/${book}`, {
+                        headers: {
+                            'Referrer-Policy': 'no-referrer-when-downgrade',
+                        },
+                    })
+                    .then(response => response.data);
+                }))
+                .then(bookData => {
+                    res.render('collection', { collection, books: bookData, verificationObject });
+                });
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });
 });
 
 router.post('/:collectionsId/edit', (req, res, next) => {
@@ -112,4 +107,5 @@ router.post('/:collectionsId/delete', async (req, res, next) => {
 		next(err);
 	}
 });
+
 module.exports = router;
